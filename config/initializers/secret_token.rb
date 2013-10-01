a06ddec290c66a62840abed2ddfdb4e484c6ecd1
Keith Laidlaw <keithlaidlaw@gmail.com>
2013-09-30 17:40:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klcom2::Application.config.secret_token = '263b5e77bfa8c59382c9c127e8fad0a9fb832ca1e5baa54da25e41af58e0711ae785fa59f28406726c21fa10664f328eea654e38fe3d4fa34078ab440fc4b1ab'
